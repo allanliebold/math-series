1c4254a5a090231067c2a4b29ef02692676f61d4
Allan Liebold <allan.liebold@gmail.com>
2017-10-17 14:01:02 -0700

    adds test case for n 5

--- test_series.py
+++ test_series.py
@@ -12,3 +12,9 @@ def test_fibonacci_first_index():
     """Test fibonacci function for n = 2."""
     from series import fibonacci
     assert fibonacci(2) == 1
+
+
+def test_fibonacci_n_5():
+    """Test fibonacci function for n = 5. The fifth fibonacci number is 3."""
+    from series import fibonacci
+    assert fibonacci(5) == 3
