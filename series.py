e82e44e1b89f85a4ce022d1c665141e4a6b83fae
David Franklin <dave5801@gmail.com>
2017-10-17 15:24:45 -0700

    second test case sum series

--- series.py
+++ series.py
@@ -19,5 +19,7 @@ def sum_series(n, first=0, second=1):
 
     if n == 1 and first ==0 and second == 1:
         return 0
+    elif first == 2 and second == 1:
+        return lucas(n)
 
     return n
